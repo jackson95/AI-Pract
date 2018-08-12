631fb945373c36aad1ebaaf9a24a2a310ca38049
jackson95 <jacksonj.mathew@gmail.com>
2018-08-12 10:54:20 +0530

    pract-1 Hellow world program

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+    This program is just a demo program of printing Hello World o the console to check whether the Java code is working
+ */
+public class HelloWorld {
+    // This function simply prints Hello World to the console
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
